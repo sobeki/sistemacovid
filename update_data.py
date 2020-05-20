bbff74d0d1f1ac81a4bd4baf53ca983a72e7df61
Filipe <filipe.fontes1212@gmail.com>
2020-05-20 11:00:07 -0300

    Inserindo com sucesso ao db, apos normalizar dados no csv.

--- update_data.py
+++ update_data.py
@@ -2,6 +2,7 @@ import requests as req
 import json
 import datetime
 import pandas as pd
+from app import insert_query
 from pathlib import Path
 from constants import GIT_API, GIT_FULL_DATA_FOLDER, DATA_FOLDER
 
@@ -13,13 +14,14 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 normalizedFileNamesList = []
 def match_Date(date_string):
     matched = False
+    
     try:
-        datetime.datetime.strptime(date_string,"%m/%d/%y")
+        dd = datetime.datetime.strptime(date_string,"%m/%d/%y")
     except Exception as e:
         matched = False
         print('Date string "'+date_string+'" nao deu match')
         return False
-    return True
+    return dd
 
 def getDataFiles():
     resp = req.get(GIT_API+GIT_FULL_DATA_FOLDER, headers=headers)
@@ -31,6 +33,20 @@ def getDataFiles():
             for obj in timeSeriesObj:
                 
                 download_file(obj)
+            if normalizedFileNamesList:
+                for eaFile in normalizedFileNamesList:
+                  
+                    df = pd.read_csv(DATA_FOLDER+'/'+eaFile)
+                    _type = checkType(eaFile)
+                    insertToDB(df, _type)
+def checkType(file_name):
+    if 'confirmed' in file_name:
+        return 'casos'
+    if 'recovered' in file_name:
+        return 'recuperados'
+    if 'deaths' in file_name:
+        return 'mortes'          
+    return False
 
 def download_file(obj):
     with req.get(obj['download_url'], stream = True) as response:
@@ -39,6 +55,23 @@ def download_file(obj):
                 f.write(chunk)
             normalizeData(obj['name'])
 
+def insertToDB(df,_type):
+    for i, row in df.iterrows():
+        pais = ""
+        for idx, val in enumerate(row):
+            data = ""
+            if "Country" in  row.index[idx]:
+                pais = val
+            else:
+                data = row.index[idx]
+
+            if (data == ""):
+                continue
+            if type(val) == float:
+                val = int(val)
+            data = match_Date(data)
+            print (data)
+            insert_query(quantidade=val,data=data, nome_pais=pais, _type=_type)
 def normalizeData(csvName):
     df = pd.read_csv(DATA_FOLDER+'/'+csvName)
     df = df.drop(df.columns[[0]], axis=1) 
@@ -48,7 +81,7 @@ def normalizeData(csvName):
     filename = 'normalized_'+csvName
     result.to_csv(DATA_FOLDER+'/'+filename)
     normalizedFileNamesList.append(filename)
-    print (normalizedFileNamesList)
+
 def getTimeSeriesObject(filesList):
     timeSeriesObj = []
     for file in filesList:
